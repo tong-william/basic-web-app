32c351152a7a5b0cd237a587c125996c15f0eb42
William Tong <wtong@andrew.cmu.edu>
2023-03-14 16:05:15 -0400

    largest number

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,5 +13,17 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("which of the following numbers is the largest")) {
+    query.replace("Which of the following numbers is the largest: ", "");
+    query.replace("?", "");
+    query.replace(",", "");
+    // 78 93 38
+    var splitstring : string[] = query.split(" ", 3);
+    
+    return (
+      String(Math.max(parseInt(splitstring[0]), parseInt(splitstring[1]), parseInt(splitstring[2])))
+    );
+  }
+
   return "";
 }
