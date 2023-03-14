e795aea30719dbd96f355b123dbf9577da1b0457
William Tong <wtong@andrew.cmu.edu>
2023-03-14 15:43:13 -0400

    What is your name query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,11 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("what is your name")) {
+    return (
+      "will"
+    );
+  }
+
   return "";
 }
